56037d5d57ab25ecee307655a130740690687143
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2023-02-02 21:39:27 +0530

    payments v1.3

--- payment/errors/index.js
+++ payment/errors/index.js
@@ -1,11 +1,9 @@
 const CustomAPIError = require('./custom-api')
-const UnauthenticatedError = require('./unauthenticated')
 const NotFoundError = require('./not-found')
 const BadRequestError = require('./bad-request')
 
 module.exports = {
   CustomAPIError,
-  UnauthenticatedError,
   NotFoundError,
   BadRequestError,
 }
