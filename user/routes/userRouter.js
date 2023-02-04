bfc6b9457f2b4968e293e0208efc5b659c3aa0ea
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2023-02-04 12:38:21 +0530

    removed authmiddleware : update user password

--- user/routes/userRouter.js
+++ user/routes/userRouter.js
@@ -13,7 +13,7 @@ router.route('/dishes/category').get(authenticationMiddleware,getDishesCategoriz
 router.route('/dishes/rating').post(authenticationMiddleware,addRating)
 
 router.route('/:uid').get(authenticationMiddleware,getUserDetails).post(authenticationMiddleware,updateUserDetails)
-router.route('/:email/updatePassword').patch(authenticationMiddleware,updatePassword)//update password using email
+router.route('/:email/updatePassword').patch(updatePassword)//update password using email
 router.route('/:uid/wallet').get(authenticationMiddleware,getBalance)
 router.route('/:email/validateOtp').post(validateOTP)
 
