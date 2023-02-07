62cb0b32599b0edd89d88ff0aac725731fe523f8
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2023-02-07 15:58:53 +0530

    django removed

--- django/first_project/first_app/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from first_app.models import orders
-# Register your models here.
-admin.site.register(orders)
--- django/first_project/first_app/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-from first_app.models import orders
-class orderserializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = orders
-        fields = ['userId','status','items','price','otp','button','paymentmode']
