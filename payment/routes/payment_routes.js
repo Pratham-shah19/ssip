8ee15b951c3b5f80a789ee09a9324fad71352196
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2023-02-07 15:34:13 +0530

    checkout  session changes

--- payment/routes/payment_routes.js
+++ payment/routes/payment_routes.js
@@ -2,9 +2,10 @@ const { application, json } = require('express')
 const express = require('express')
 const router = express.Router()
 
-const {paymentsheet,webhook} = require('../controllers/payments')
+const {paymentsheet,webhook,createcheckoutsession} = require('../controllers/payments')
 
 router.route('/payment-sheet').post(paymentsheet)
 router.route('/webhook').post(express.raw({type:application/json}),webhook)
+router.route('create-checkout-session').post(createcheckoutsession);
 
 module.exports = router
