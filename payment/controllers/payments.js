f1ee11c7f0ae4ab9d6db2c4824ecc288c1dcc932
Pratham-shah19 <83119582+Pratham-shah19@users.noreply.github.com>
2023-02-09 20:58:17 +0530

    payment controller

--- payment/controllers/payments.js
+++ payment/controllers/payments.js
@@ -1,34 +1,36 @@
-const Users = require('../models/User')
-const Canteen = require('../models/Canteen')
-const Basket = require('../models/Basket')
-const Dish= require('../models/Dish')
-const Orders = require("../models/Orders")
+const Users = require("../models/User");
+const Canteen = require("../models/Canteen");
+const Basket = require("../models/Basket");
+const Dish = require("../models/Dish");
+const Orders = require("../models/Orders");
 
 const stripe = require("stripe")(
   "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"
 );
-const endpointSecret = "whsec_fb5686e7f586c42bd82e5e7a0839f44d5dd6c582ee7cf1fcce0f7be84d306fa1";
-const pubkey = "pk_live_51KyqwvSFXhJBixXAkcyirlXABSuwuQoC9a6daIFPkc7mrRotk18Xe1eISkB7tFR1krgUbuw8FY6SQxvmTx9ZZ89100S4jkwTWc";
+const endpointSecret =
+  "whsec_fb5686e7f586c42bd82e5e7a0839f44d5dd6c582ee7cf1fcce0f7be84d306fa1";
+const pubkey =
+  "pk_live_51KyqwvSFXhJBixXAkcyirlXABSuwuQoC9a6daIFPkc7mrRotk18Xe1eISkB7tFR1krgUbuw8FY6SQxvmTx9ZZ89100S4jkwTWc";
 
-const YOUR_DOMAIN = "http://localhost:6990"//put react's port number...
+const YOUR_DOMAIN = "http://localhost:6990"; //put react's port number...
 
-const createcheckoutsession = async(req, res) => {
-  const canteen = await Canteen.find({})
+const createcheckoutsession = async (req, res) => {
+  const canteen = await Canteen.find({});
   const wallet = canteen[0].wallet;
   var date = new Date();
   const day = date.getDate();
-  const month = date.getMonth()+1;
+  const month = date.getMonth() + 1;
   const year = date.getFullYear();
-  date = day+'/'+month+'/'+year;
+  date = day + "/" + month + "/" + year;
   const product = await stripe.products.create({
-    name:date, 
-  })
+    name: date,
+  });
   const price = await stripe.prices.create({
-    unit_amount:wallet * 100,
-    currency:'inr',
-    product:product.id
-  })
-  
+    unit_amount: wallet * 100,
+    currency: "inr",
+    product: product.id,
+  });
+
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
@@ -36,7 +38,6 @@ const createcheckoutsession = async(req, res) => {
         price: price.id,
         quantity: 1,
       },
-      
     ],
     mode: "payment",
     success_url: `${YOUR_DOMAIN}?success=true`,
@@ -49,7 +50,7 @@ const createcheckoutsession = async(req, res) => {
 const paymentsheet = async (req, res) => {
   // Use an existing Customer ID if this is a returning customer.
   var { price, userId } = req.body;
-  const customer = await Users.findOne({_id:userId})
+  const customer = await Users.findOne({ _id: userId });
 
   const ephemeralKey = await stripe.ephemeralKeys.create(
     { customer: customer.customerId },
@@ -67,83 +68,48 @@ const paymentsheet = async (req, res) => {
     paymentIntent: paymentIntent.client_secret,
     ephemeralKey: ephemeralKey.secret,
     customer: customer.customerId,
-    publishableKey:pubkey,
+    publishableKey: pubkey,
   });
 };
 
 const payCanteen = async (req, res) => {
-    const { customer ,amount_total} = req.body;
-    const user = await Users.findOne({ customerId: customer });
-    if (!user) {
-      throw new BadRequestError("Invalid userid");
-    }
-  
-    //adding coins to canteen's wallet
-    const canteen = await Canteen.findOne({ name: 'Sachivalaya' });
-    if (!canteen) {
-      throw new BadRequestError("Invalid canteen name");
+  const { uid } = req.params;
+  const user = await Users.findOne({ _id: uid });
+  //canteen
+  const canteen = await Canteen.findOne({ name: "Sachivalaya" });
+  //user basket
+  const basket = await Basket.findOne({ userId: uid });
+  const price = basket.price;
+  //deducting price from user's wallet
+  const deduct = await Users.findOneAndUpdate(
+    { _id: uid },
+    { wallet: user.wallet - price },
+    { new: true, runValidators: true }
+  );
+  //adding coins to canteen's wallet
+  const pay = await Canteen.findOneAndUpdate(
+    { name: "Sachivalaya" },
+    { wallet: canteen.wallet + price },
+    { new: true, runValidators: true }
+  );
+  //generating otp
+  const otp = Math.floor(Math.random() * 10000);
+  const orderObj = {};
+  orderObj.items = basket.items;
+  orderObj.userId = basket.userId;
+  orderObj.price = price;
+  orderObj.otp = otp;
+  //creating order
+  const order = await Orders.create(orderObj);
+  //deleting basket
+  const emptyBasket = await Basket.findOneAndDelete({ userId: uid });
 
-    }
-    const pay = await Canteen.findOneAndUpdate(
-      { name: 'Sachivalaya'},
-      { onlinewallet: canteen.onlinewallet + amount_total/100 },
-      { new: true, runValidators: true }
-    );
-    const basket = await Basket.findOne({ userId: user._id });
-    if (!basket) {
-      throw new BadRequestError("Invalid user id, could not find basket");
-    }
-    var items = basket.items;
-    items.forEach(async (e) => {
-      let obj = {};
-      const dish = await Dish.findOne({ _id: e.dishId, isAvailable: true });
-      if (!dish) {
-        res
-          .status(StatusCodes.OK)
-          .json({ res: "fail", data: "dish is not actually available" });
-      } else {
-        if (dish?.quantity >= e.qty) {
-          obj.dishId = e.dishId;
-          obj.qty = dish?.quantity - e.qty;
-          if (obj.qty < 10) {
-            const dish = await Dish.findOneAndUpdate(
-              { _id: obj.dishId },
-              { quantity: obj.qty, isAvailable: false },
-              { runValidators: true, new: true }
-            );
-          } else {
-            const dish = await Dish.findOneAndUpdate(
-              { _id: obj.dishId },
-              { quantity: obj.qty },
-              { runValidators: true, new: true }
-            );
-          }
-        } else {
-          res
-            .status(StatusCodes.OK)
-            .json({ res: "fail", data: "not enough quantity" });
-        }
-      }
-    });
-  
-    //generating otp
-    const otp = Math.floor(Math.random() * 10000);
-    const orderObj = {};
-    orderObj.items = basket.items;
-    orderObj.userId = basket.userId;
-    orderObj.price = price;
-    orderObj.otp = otp;
-    orderObj.paymentmode = "ONLINE";
-    //creating order
-    const order = await Orders.create(orderObj);
-    //deleting basket
-    const emptyBasket = await Basket.findOneAndDelete({ userId: user._id });
-  
-    res.status(StatusCodes.CREATED).json({
-      res: "success",
-      data: { "current balance": deduct.wallet, orderOtp: otp },
-    });
-  };
+  res.status(StatusCodes.CREATED).json({
+    res: "success",
+    data: { "current balance": deduct.wallet, orderOtp: otp },
+  });
+};
+
 const webhook = async (req, res) => {
   let event = req.body;
   // Only verify the event if you have an endpoint secret defined.
@@ -170,7 +136,7 @@ const webhook = async (req, res) => {
       console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
       // Then define and call a method to handle the successful payment intent.
       // handlePaymentIntentSucceeded(paymentIntent);
-      payCanteen(req,res)
+      payCanteen(req, res);
       break;
     case "payment_method.attached":
       const paymentMethod = event.data.object;
@@ -186,4 +152,4 @@ const webhook = async (req, res) => {
   res.status(200).send("order placed");
 };
 
-module.exports = {paymentsheet,webhook,createcheckoutsession}
+module.exports = { paymentsheet, webhook, createcheckoutsession };
